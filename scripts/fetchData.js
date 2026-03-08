562fbaec188f3f8b19e5e02c82e9b211c4431e26
azizu06 <ab725492@ucf.edu>
2026-03-08 19:17:20 -0400

    create node script for fetching launcher and spacecraft info

---
+++ scripts/fetchData.js
@@ -0,0 +1,51 @@
+import fs from "fs/promises";
+
+const LAUNCHER_URL =
+  "https://ll.thespacedevs.com/2.3.0/launcher_configurations/?format=json&limit=100";
+const SPACECRAFT_URL =
+  "https://ll.thespacedevs.com/2.3.0/spacecraft_configurations/?format=json&limit=100";
+
+function isValidItem(item, type) {
+  const origin = type === "launcher" ? "manufacturer" : "agency";
+  return (
+    item && item.id && item.name && item[origin]?.name && item.image?.image_url
+  );
+}
+
+function formatItem(item, type) {
+  const origin = type === "launcher" ? "manufacturer" : "agency";
+  return {
+    id: `${type}-${item.id}`,
+    name: item.name,
+    manufacturer: item[origin].name,
+    image: item.image.image_url,
+    price: Math.floor(Math.random() * 50000000) + 10000000,
+  };
+}
+
+async function fetchData() {
+  const [launcherRaw, spacecraftRaw] = await Promise.all([
+    fetch(LAUNCHER_URL),
+    fetch(SPACECRAFT_URL),
+  ]);
+  const launchersData = await launcherRaw.json();
+  const spacecraftsData = await spacecraftRaw.json();
+
+  const launchers = launchersData.results
+    .filter((item) => isValidItem(item, "launcher"))
+    .slice(0, 15)
+    .map((item) => formatItem(item, "launcher"));
+
+  const spacecrafts = spacecraftsData.results
+    .filter((item) => isValidItem(item, "spacecraft"))
+    .slice(0, 15)
+    .map((item) => formatItem(item, "spacecraft"));
+
+  const products = [...launchers, ...spacecrafts];
+  await fs.writeFile(
+    "./src/data/products.json",
+    JSON.stringify(products, null, 2),
+  );
+}
+
+fetchData();
