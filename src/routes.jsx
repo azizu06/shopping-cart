1dbcf662191ea31f133bcb66c36fb581d1162a96
azizu06 <ab725492@ucf.edu>
2026-03-09 18:22:15 -0400

    fix routes to be a var not function

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,25 +3,23 @@ import Shop from "./pages/Shop";
 import Cart from "./pages/Cart";
 import Layout from "./components/Layout";
 
-export const router = () => {
-  [
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        {
-          index: true,
-          element: <Home />,
-        },
-        {
-          path: "/shop",
-          element: <Shop />,
-        },
-        {
-          path: "/cart",
-          element: <Cart />,
-        },
-      ],
-    },
-  ];
-};
+export const routes = [
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/shop",
+        element: <Shop />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+];
