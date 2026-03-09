97b9b70c0389800397ae6ab560b49beec49a4f9d
azizu06 <ab725492@ucf.edu>
2026-03-09 17:27:39 -0400

    filter and sort options as const

---
+++ src/const.js
@@ -0,0 +1,49 @@
+export const sortFns = {
+  priceAsc: (a, b) => a.price - b.price,
+  priceDesc: (a, b) => b.price - a.price,
+  nameAsc: (a, b) => a.name.localCompare(b.name),
+  nameDesc: (a, b) => b.name.localCompare(a.name),
+};
+
+export const sortOptions = [
+  {
+    label: "Reccomended",
+    value: "default",
+  },
+  {
+    label: "Price: Low to High",
+    value: "priceAsc",
+  },
+  {
+    label: "Price: High to Low",
+    value: "priceDesc",
+  },
+  {
+    label: "Alphabetical: A-Z",
+    value: "nameAsc",
+  },
+  {
+    label: "Alphabetical: Z-A",
+    value: "nameDesc",
+  },
+];
+
+export const filterFns = {
+  launchers: (p) => p.type === "launcher",
+  spacecrafts: (p) => p.type === "spacecraft",
+};
+
+export const filterOptions = [
+  {
+    label: "All",
+    value: "default",
+  },
+  {
+    label: "Launcher",
+    value: "launchers",
+  },
+  {
+    label: "Spacecraft",
+    value: "spacecrafts",
+  },
+];
