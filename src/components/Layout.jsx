66e49f3884b7f2a7a78f6103c899a4ebaf025e17
azizu06 <ab725492@ucf.edu>
2026-03-09 14:07:36 -0400

    create default layout for pages

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./Nav";
+import { Outlet } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <NavBar />
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
