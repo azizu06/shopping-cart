788db1d9428a96fc7e08c169089708716c2eb9bb
azizu06 <ab725492@ucf.edu>
2026-03-09 17:45:35 -0400

    add cart context for app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
 import { createBrowserRouter, Route, RouterProvider } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { router } from "./routes";
+import { router } from "../routes";
+import { CartProvider } from "../context/CartProvider";
 
 const queryClient = new QueryClient();
 const routers = createBrowserRouter(router);
 
 const App = () => {
   <QueryClientProvider client={queryClient}>
-    <RouterProvider router={routers} />
+    <CartProvider>
+      <RouterProvider router={routers} />
+    </CartProvider>
   </QueryClientProvider>;
 };
 export default App;
