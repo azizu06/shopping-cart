cfe7588bba69c5ceac617c601c536db50e898358
azizu06 <ab725492@ucf.edu>
2026-03-09 13:39:14 -0400

    Add router and query client for app

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import routes from "./routes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { router } from "./routes";
 
-const router = createBrowserRouter(routes);
+const queryClient = new QueryClient();
+const routers = createBrowserRouter(router);
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <RouterProvider router={routers} />
+    </StrictMode>
+  </QueryClientProvider>,
 );
