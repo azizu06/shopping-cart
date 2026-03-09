2ba21322d58a5d9c5552bb393ae36f294069d21f
azizu06 <ab725492@ucf.edu>
2026-03-09 17:45:49 -0400

    init home page for shop

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className="flex flex-col gap-5">
+      <p className="text-4xl">All your space equipment, carefully curated</p>
+      <button className="text-lg cursor-pointer p-4 bg-blue-300">
+        Explore the shop
+      </button>
+    </div>
+  );
+};
+
+export default Home;
