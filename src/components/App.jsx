7f650652451e8f8d94d2ba467a41084d7d35c3bc
azizu06 <ab725492@ucf.edu>
2026-03-09 18:22:49 -0400

    forgot to return jsx from app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
-import { createBrowserRouter, Route, RouterProvider } from "react-router";
+import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { router } from "../routes";
+import { routes } from "../routes";
 import { CartProvider } from "../context/CartProvider";
 
 const queryClient = new QueryClient();
-const routers = createBrowserRouter(router);
+const router = createBrowserRouter(routes);
 
 const App = () => {
-  <QueryClientProvider client={queryClient}>
-    <CartProvider>
-      <RouterProvider router={routers} />
-    </CartProvider>
-  </QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <CartProvider>
+        <RouterProvider router={router} />
+      </CartProvider>
+    </QueryClientProvider>
+  );
 };
 export default App;
