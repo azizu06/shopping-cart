5fa5b6b03d102f59371c5d3f8ef64d141c997dbb
azizu06 <ab725492@ucf.edu>
2026-03-09 17:27:26 -0400

    init version of reusable dropdown for organizing shopping items

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { ChevronUp, ChevronDown } from "lucide-react";
+
+const DropDown = (options, value, setOption) => {
+  const [open, setOpen] = useState(false);
+  const selected = options.find((opt) => opt.value === value);
+
+  return (
+    <div className="relative">
+      <div className="flex justify-between">
+        <button onClick={() => setOpen(!open)}>
+          {selected?.label || "Select an Option"}
+        </button>
+        {open ? <ChevronUp /> : <ChevronDown />}
+      </div>
+      {open && (
+        <div className="flex flex-col absolute">
+          {options.map((opt) => (
+            <button
+              key={opt.value}
+              onClick={() => setOption(opt.value)}
+              className="flex justify-center p-2"
+            >
+              {opt.label}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DropDown;
