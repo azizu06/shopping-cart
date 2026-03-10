7b73c628a1a50d567efe398f70fa7c3af2ec6d54
azizu06 <ab725492@ucf.edu>
2026-03-10 14:57:23 -0400

    add cart quantity indicator

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
+import { CartContext } from "../context/CartContext";
+import { useContext } from "react";
+import { ShoppingCart } from "lucide-react";
 
 const NavBar = () => {
-  const btnStyle = "cursor-pointer p-4 hover:bg-blue-300";
+  const { cart } = useContext(CartContext);
+  const cartCount = Object.values(cart).reduce((total, val) => total + val, 0);
+  const btnStyle =
+    "cursor-pointer p-4 hover:bg-blue-300 gap-2 flex items-center";
   return (
     <div className="fixed inset-x-0 top h-16 z-50">
-      <nav className="flex w-screen backdrop-blur justify-between gap-6 rounded-l border px-10 py-2">
+      <nav className="flex w-screen backdrop-blur justify-between gap-6 rounded-l border px-12 py-2">
         <button className="cursor-pointer">SpaceShop</button>
         <div className="flex gap-5">
           <Link to="/" className={btnStyle}>
@@ -13,8 +19,14 @@ const NavBar = () => {
           <Link to="/shop" className={btnStyle}>
             Shop
           </Link>
-          <Link to="/cart" className={btnStyle}>
+          <Link to="/cart" className={`${btnStyle} relative`}>
+            <ShoppingCart size={18} />
             Cart
+            {cartCount > 0 && (
+              <span className="absolute rounded-full flex items-center justify-center w-6.75 h-6.5 -top-0.5 -right-3 bg-red-500 text-xs">
+                {cartCount > 99 ? "99+" : cartCount}
+              </span>
+            )}
           </Link>
         </div>
       </nav>
