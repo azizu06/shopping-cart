707fb66bd68388f5c1e94401c223af3a399a4835
azizu06 <ab725492@ucf.edu>
2026-03-10 18:19:26 -0400

    add min for stepper and send back count value instead of setting in stepper

--- src/components/QtyStepper.jsx
+++ src/components/QtyStepper.jsx
@@ -1,27 +1,26 @@
 import { Plus, Minus } from "lucide-react";
 
-const QtyStepper = ({ setCount, count, id }) => {
+const QtyStepper = ({ setCount, count, min }) => {
   return (
     <>
       <div className="flex items-center border">
         <button
           type="button"
           className="flex h-8 w-8 shrink-0 items-center justify-center"
-          onClick={() => setCount(id, (c) => Math.max(1, c - 1))}
+          onClick={() => setCount(Math.max(min, count - 1))}
         >
           <Minus size={16} />
         </button>
         <input
           type="number"
           value={count}
-          onChange={(e) => setCount(id, Number(e.target.value))}
+          onChange={(e) => setCount(Math.max(min, Number(e.target.value)))}
           className="no-spinner w-16 flex-none border-x text-center"
-          min="1"
         />
         <button
           type="button"
           className="flex h-8 w-8 shrink-0 items-center justify-center"
-          onClick={() => setCount(id, (c) => c + 1)}
+          onClick={() => setCount(count + 1)}
         >
           <Plus size={16} />
         </button>
