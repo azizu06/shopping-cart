3b2e382ae829d2e92ce68ec7942fd036287ff7f4
azizu06 <ab725492@ucf.edu>
2026-03-10 18:51:47 -0400

    remove unused router import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, Route, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { routes } from "../routes";
 import { CartProvider } from "../context/CartProvider";
