b1be199d8f34da006177db42a8d89f128b7299f6
azizu06 <ab725492@ucf.edu>
2026-03-10 18:16:31 -0400

    fix callback setter from qty stepper and add min count

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -2,21 +2,27 @@ import { Trash } from "lucide-react";
 import QtyStepper from "./QtyStepper";
 
 const CartItem = ({ item, removeItem, updateCount, cart }) => {
+  if (!item) return null;
   const itemTotal = item.price * cart[item.id];
   return (
-    <div className="flex justify-between">
-      <div className="flex gap-2">
-        <img src={item.image} alt={item.name} className="aspect-4/3" />
+    <div className="flex gap-4 w-full border p-4">
+      <img src={item.image} alt={item.name} className="w-30 h-30" />
+      <div className="flex flex-col gap-8">
         <div className="flex flex-col gap-1">
           <p>{item.name}</p>
           <p>
             ${item.price} each (Subtotal: ${itemTotal})
           </p>
         </div>
-      </div>
-      <div className="flex gap-2">
-        <QtyStepper setCount={updateCount} count={cart[item.id]} id={item.id} />
-        <Trash size={18} onClick={removeItem} />
+        <div className="flex gap-2 items-center">
+          <QtyStepper
+            setCount={(newCount) => updateCount(item.id, newCount)}
+            count={cart[item.id]}
+            id={item.id}
+            min={1}
+          />
+          <Trash size={24} onClick={() => removeItem(item.id)} />
+        </div>
       </div>
     </div>
   );
